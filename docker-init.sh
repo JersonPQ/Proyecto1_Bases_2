2f078625dc6f526055a47c2c97703b41d3b13ee9
Whatsername5870 <ma.brenes@estudiantec.cr>
2024-06-18 01:31:13 -0600

    a

--- docker-init.sh
+++ docker-init.sh
@@ -10,5 +10,8 @@ superset fab create-admin --username admin --firstname Admin --lastname User --e
 # Initialize Superset
 superset init
 
+# Import the dashboard
+superset import_dashboards -p /superset-exports/dashboard.zip
+
 # Start the Superset server
 superset run -h 0.0.0.0 -p 8088 --with-threads --reload --debugger
